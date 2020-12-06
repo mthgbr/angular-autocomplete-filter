2d077f5604dba2f502770368652932b19ef6dfef
mthgbr <mathieu.gebauer@gmail.com>
2020-12-06 17:46:49 +0100

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
-import { FilterComponent } from './filter-component/filter.component';
-import { Filters, FilterObject } from './filter-component/filter.class';
+import { Component } from "@angular/core";
+import { FilterComponent } from "./filter-component/filter.component";
+import { Filters, FilterObject } from "./filter-component/filter.class";
+import { MatTableDataSource } from "@angular/material/table";
 
 export interface MatData {
   name: string;
@@ -9,27 +10,26 @@ export interface MatData {
 }
 
 const MAT_DATA: MatData[] = [
-  {name: 'Mathieu', age: 29, city: 'Toulouse'},
-  {name: 'Grégoire', age: 59, city: 'Limoge'},
-  {name: 'Jean-Louis', age: 2, city: 'Paris'}
+  { name: "Mathieu", age: 29, city: "Toulouse" },
+  { name: "Grégoire", age: 59, city: "Limoge" },
+  { name: "Jean-Louis", age: 2, city: "Paris" }
 ];
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  selector: "my-app",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
-export class AppComponent  {
-  dataSource = null;
+export class AppComponent {
+  dataSource = new MatTableDataSource<any>(MAT_DATA);
   displayedColumns = [];
 
   constructor() {
-    this.dataSource.data = MAT_DATA;
     this.displayedColumns = Object.keys(MAT_DATA[0]);
   }
 
   applyFilter(filters: Filters) {
     /** trigger default behaviour for filtering MatTableDataSource's data */
-    this.dataSource.filter = filters;
+    // this.dataSource.filter = filters;
   }
 }
