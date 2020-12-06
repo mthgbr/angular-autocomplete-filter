4673cd3ba44fafa630c8d5ca2f69259e9d652792
mthgbr <mathieu.gebauer@gmail.com>
2020-12-06 17:36:29 +0100

    fix MatTableModule import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {
-  MatTableModule
-} from '@angular/material';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { MatTableModule } from "@angular/material/table";
 
-import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
-import { FilterComponent } from './filter-component/filter.component';
+import { AppComponent } from "./app.component";
+import { HelloComponent } from "./hello.component";
+import { FilterComponent } from "./filter-component/filter.component";
 
 @NgModule({
-  imports:      [ 
-    BrowserModule, 
-    FormsModule, 
-    ReactiveFormsModule,
-    MatTableModule
-  ],
-  declarations: [ AppComponent, HelloComponent, FilterComponent ],
-  bootstrap:    [ AppComponent ],
-  exports:      [ FilterComponent ],
+  imports: [BrowserModule, FormsModule, ReactiveFormsModule, MatTableModule],
+  declarations: [AppComponent, HelloComponent, FilterComponent],
+  bootstrap: [AppComponent],
+  exports: [FilterComponent]
 })
-export class AppModule { }
+export class AppModule {}
